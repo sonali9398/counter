c3c9ed29866867a1d32ccadcb95a56175e44b0d9
sonali9398 <sonalikarangutkar9398@gmail.com>
2023-12-28 21:21:58 +0530

    Redux Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, incrementByAmount} from './app/Slice';
 
 function App() {
+
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <span>{count}</span>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+      <input
+        type='number'
+      />
+      <button onClick={() => dispatch(incrementByAmount())}>add</button>
+  
     </div>
   );
 }
---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './Slice'
+
+export default configureStore({
+    reducer:{
+        counter: counterReducer,
+    },
+})
